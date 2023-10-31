c34396129b55b9c5a6b7a0d9d25f94ca5bdb3f05
indrajeet.ambadekar <indrajeet.ambadekar@gmail.com>
2023-10-31 22:22:07 +0530

    pulling latest

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import { AppHeader, Text, Avatar } from "elysium-cloud-ui";
 import CompanyLogo from "..//assets/images/JFH_Start-Restart-Rise.png";
 const Header = ({ ...props }) => (
   <AppHeader
-    className="app-header"
+    className='app-header'
     logo={
-      <div className="logo-cntnr">
-        <img src={CompanyLogo} alt="JobsForHer" />
-        <i className="fas fa-bars" />
+      <div className='logo-cntnr'>
+        <img src={CompanyLogo} alt='JobsForHer' />
+        <i className='fas fa-bars' />
       </div>
     }
     secondaryIcon={null}
@@ -14,16 +14,16 @@ const Header = ({ ...props }) => (
       console.log("Header Icon Clicked");
     }}
     profileIcon={
-      <Avatar size="L">
-        <div className="user-avatar"></div>
+      <Avatar size='L'>
+        <div className='user-avatar'></div>
       </Avatar>
     }
   >
-    <div className="nav-cntnr">
-      <Text type="h4">Dashboard</Text>
-      <Text type="h4">Packages</Text>
-      <Text type="h4">Events</Text>
-      <Text type="h4">Blogs</Text>
+    <div className='nav-cntnr'>
+      <Text type='h4'>Dashboard</Text>
+      <Text type='h4'>Packages</Text>
+      <Text type='h4'>Events</Text>
+      <Text type='h4'>Blogs</Text>
     </div>
   </AppHeader>
 );
